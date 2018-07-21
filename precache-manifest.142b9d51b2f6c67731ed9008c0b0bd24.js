a27a1ab24da2badc2a56905291725b0746525bd0
Roy Portas <royportas@gmail.com>
2018-07-21 11:48:35 +1000

    Updates

---
+++ precache-manifest.142b9d51b2f6c67731ed9008c0b0bd24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c103fd3ee057e11d36b8",
+    "url": "/js/chunk-vendors.c103fd3e.js"
+  },
+  {
+    "revision": "3e358d0b0299a8150068",
+    "url": "/js/app.3e358d0b.js"
+  },
+  {
+    "revision": "2d549a5c7f30b8bb67037fffd9909516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62dc5d5a6e323401629de642a9810019",
+    "url": "/img/background.jpeg"
+  },
+  {
+    "revision": "daf4ce5f4bc17ebcdf060bf2d83ed0ed",
+    "url": "/data/skills.json"
+  },
+  {
+    "revision": "14f0a9c316e2f6c42bbd0d6ecb142140",
+    "url": "/data/projects.json"
+  },
+  {
+    "revision": "9c38346ad19a9c81a67ea80c6a213cf0",
+    "url": "/data/jobs.json"
+  },
+  {
+    "revision": "62ac5f02b4523bf0726b097998f3e02a",
+    "url": "/data/education.json"
+  },
+  {
+    "revision": "3e358d0b0299a8150068",
+    "url": "/css/app.f77a9e96.css"
+  }
+];
